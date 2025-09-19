58b17c756ce9117dfc8136b677ece8a4f9c1e058
An Kun <ankun.n.m@gmail.com>
2025-09-19 16:13:56 +0700

    main 2.1.0

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -5,6 +5,6 @@ import { Database } from "./types";
 export function createClient() {
   return createBrowserClient<Database>(
     env.NEXT_PUBLIC_SUPABASE_URL,
-    env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   );
 }
