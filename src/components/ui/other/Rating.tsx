14f88c043327cd265ec7ac8c0502d2b75916f04a
An Kun <ankun.n.m@gmail.com>
2025-11-12 12:12:13 +0700

    Chỉnh sửa giao diện

--- src/components/ui/other/Rating.tsx
+++ src/components/ui/other/Rating.tsx
@@ -8,11 +8,17 @@ export interface RatingProps {
 
 const Rating: React.FC<RatingProps> = ({ rate = 0, count = 0 }) => {
   return (
-    <div className="flex items-center gap-1 font-semibold text-warning-500">
-      <Star />
-      <p>
-        {rate.toFixed(1)} {count > 0 && `(${formatNumber(count)})`}
-      </p>
+    <div className="flex items-center gap-1 font-semibold">
+      <span className="text-cyan-500">TMDB</span>
+      <Star className="text-cyan-500" />
+      <span className="text-warning-500">
+        {rate.toFixed(1)}
+      </span>
+      {count > 0 && (
+        <span className="text-default-500 font-normal">
+          ({formatNumber(count)})
+        </span>
+      )}
     </div>
   );
 };
