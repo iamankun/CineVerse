2bc149db394c3b8a3d2f63793a77fd02d6b85ab9
An Kun <ankun.n.m@gmail.com>
2026-06-14 01:24:05 +0700

    Phiên bản 1.3.7

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/logo-cineverse.webp',
         sizes: '192x192',
-        type: 'image/png',
+        type: 'image/webp',
       },
       {
-        src: '/icon-512x512.png',
+        src: '/logo-cineverse.webp',
         sizes: '512x512',
-        type: 'image/png',
+        type: 'image/webp',
       },
     ],
   };
